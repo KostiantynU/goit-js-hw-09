b31e1bf36fb6f50264201eec555c9de4252a654c
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-04-10 00:06:59 +0300

    begin third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,33 @@
+import Notiflix from 'notiflix';
+const refs = {
+  formEl: document.querySelector('.form'),
+  delayEl: null,
+  stepEl: null,
+  amountEl: null,
+};
+
+refs.formEl.addEventListener('input', beginOfWork);
+refs.formEl.addEventListener('submit', beforeCratePromise);
+
+function beginOfWork({ target }) {
+  if (target.name === 'delay') {
+    refs.delayEl = target.value;
+  } else if (target.name === 'step') {
+    refs.stepEl = target.value;
+  } else if (target.name === 'amount') {
+    refs.amountEl = target.value;
+  }
+}
+function beforeCratePromise(event) {
+  event.preventDefault();
+  for (let i = 0; i <= refs.amountEl; i += 1) {
+    // console.log(refs.delayEl);
+    // console.log(refs.amountEl);
+    // console.log(refs.stepEl);
+    createPromise(i, refs.delayEl, refs.stepEl);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
