3b7f9ee0cd85309f19d28309b09719114ef683a0
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-04-11 23:14:34 +0300

    third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,28 +11,42 @@ refs.formEl.addEventListener('submit', beforeCratePromise);
 
 function beginOfWork({ target }) {
   if (target.name === 'delay') {
-    refs.delayEl = target.value;
+    refs.delayEl = Number(target.value);
   } else if (target.name === 'step') {
-    refs.stepEl = target.value;
+    refs.stepEl = Number(target.value);
   } else if (target.name === 'amount') {
-    refs.amountEl = target.value;
+    refs.amountEl = Number(target.value);
   }
 }
 function beforeCratePromise(event) {
   event.preventDefault();
-  for (let i = 0; i <= refs.amountEl; i += 1) {
-    // console.log(refs.delayEl);
-    // console.log(refs.amountEl);
-    // console.log(refs.stepEl);
-    createPromise(i, refs.delayEl, refs.stepEl);
+  for (let i = 1; i <= refs.amountEl; i += 1) {
+    createPromise(i, refs.delayEl);
+    refs.delayEl += refs.stepEl;
   }
 }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        // Fulfill
+      } else {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        // Reject
+      }
+    }, delay);
+  });
+  return promise;
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
